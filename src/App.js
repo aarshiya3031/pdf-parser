48ea3d39f4162341f11cb7e7e41501abff7c9b54
aarshiya3031 <aarshiya3031@gmail.com>
2024-02-15 23:05:02 +0530

    feat/add code for parsing

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import PdfUploader from "./Uploader.js";
+function App() {
+  return (
+    <div>
+      <PdfUploader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Uploader.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { pdfjs } from "react-pdf";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import "./index.css";
+
+export default function PdfUploader() {
+  const [pdfFile, setPdfFile] = useState(null);
+  const [textContent, setTextContent] = useState("");
+  const [iconLabel, setIconLabel] = useState(true);
+
+  const onFileChange = (event) => {
+    const file = event.target.files[0];
+
+    setPdfFile(file);
+    setTextContent(""); // Reset text content when a new file is selected
+
+    if (file) {
+      setIconLabel(false);
+      // Read the PDF file and extract text content
+      const reader = new FileReader();
+      reader.onload = () => {
+        pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+        extractTextContent(reader.result);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
+  const extractTextContent = async (fileData) => {
+    const pdf = await pdfjs.getDocument({ data: fileData }).promise;
+    console.log(pdf);
+    const pdfTextArray = [];
+    for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++) {
+      const page = await pdf.getPage(pageNumber);
+      const textContent = await page.getTextContent();
+      const pageText = textContent.items.map((item) => item.str).join("");
+      pdfTextArray.push(pageText);
+    }
+    setTextContent(pdfTextArray.join("\n"));
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        flexDirection: "column",
+        backgroundColor: "#dc92fc",
+      }}
+    >
+      <h1 style={{ fontFamily: "Poppins, sans-serif" }}>
+        Extract text From PDF!
+      </h1>
+      <div
+        style={{
+          backgroundColor: "white",
+          marginTop: "0px",
+          minWidth: "700px",
+          minHeight: "250px",
+          maxHeight: "1050px",
+          maxWidth: "1000px",
+          borderRadius: "15px",
+          padding: "20px",
+        }}
+      >
+        {iconLabel && (
+          <label
+            htmlFor="file-upload"
+            style={{
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <CloudUploadIcon sx={{ fontSize: 100 }} color="primary" />
+            <input
+              type="file"
+              id="file-upload"
+              onChange={onFileChange}
+              accept=".pdf"
+              style={{ display: "none" }}
+            />
+            <h2 style={{ fontFamily: "Poppins, sans-serif" }}>
+              Drop your files here
+            </h2>
+          </label>
+        )}
+        {pdfFile && (
+          <div>
+            <h1
+              style={{
+                fontFamily: "Poppins, sans-serif",
+                marginTop: "0px",
+                marginBottom: "5px",
+              }}
+            >
+              Text Content:
+            </h1>
+            <div style={{ whiteSpace: "pre-wrap" }}>{textContent}</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
